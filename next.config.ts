3627be1f12a74798104da0e046e3d098921c9d04
Fathan Nabil Arrazani <fathanamanda12@gmail.com>
2026-08-30 17:18:43 +0700

    Fix GitHub Pages CSS path and add .nojekyll

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/dompetku",
+  assetPrefix: "/dompetku",
   images: {
     unoptimized: true,
   },
